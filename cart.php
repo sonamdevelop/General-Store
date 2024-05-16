9c22ff5888489ecb5e00b66719c5744c29edf396
Mohit Pandey <93579686+22Mohit22@users.noreply.github.com>
2024-05-16 10:33:53 +0530

    changed html files to php

---
+++ cart.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Cart</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="cart d-flex justify-content-around align-items-center">
+        <div class="cart-products">
+            <div class="row row-cols-1 row-cols-md-2 mx-2 my-2 g-5" style="width: 40rem;">
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="./images/product.png" class="card-img-top" alt="product image">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Shirt
+                            </h5>
+                            <p class="card-text">
+                                Classic yet contemporary, our shirt features timeless elegance with a modern twist, crafted from premium materials for comfort and style.
+                            </p>
+                            <p class="card-text">
+                                Price: &#x20B9;500
+                            </p>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                <button class="btn btn-primary me-md-2" type="button">Qty: 2</button>
+                            </div>
+                        </div>
+                    </div>  
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="./images/product.png" class="card-img-top" alt="product image">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Shirt
+                            </h5>
+                            <p class="card-text">
+                                Classic yet contemporary, our shirt features timeless elegance with a modern twist, crafted from premium materials for comfort and style.
+                            </p>
+                            <p class="card-text">
+                                Price: &#x20B9;500
+                            </p>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                <button class="btn btn-primary me-md-2" type="button">Qty: 2</button>
+                            </div>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
+        <div class="cart-checkout d-flex flex-column" style="width: 30rem;">
+            <h1 class="title">Total products: 4</h1>
+            <h1 class="title">Total price: 2000</h1>
+            <h1 class="title">Discount: 10%</h1>
+            <h1 class="title">Final price: 1800</h1>
+            <button type="button" class="btn btn-outline-dark my-2 fs-4">Add more items</button>
+            <button type="button" class="btn btn-dark my-2 fs-4">Checkout Now</button>
+        </div>
+    </div>
+</body>
+</html>
